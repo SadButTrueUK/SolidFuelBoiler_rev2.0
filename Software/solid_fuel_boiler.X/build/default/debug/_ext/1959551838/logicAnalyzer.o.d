0d73537c8fcc3ad09aa6e0a9e62c1a9ded2939d8
Vladislav Kruglikov <juffin.v@gmail.com>
2020-08-18 21:48:16 +0300

    Changed Revision_history.txt

--- Software/solid_fuel_boiler.X/build/default/debug/_ext/1959551838/logicAnalyzer.o.d
+++
@@ -1,7 +0,0 @@
-build/default/debug/_ext/1959551838/logicAnalyzer.o:  \
-	D:/Vl_prj/Solid_fuel_boiler(rev\ 2_0)/Software/src/DebugTools/logicAnalyzer.c  \
-	D:/Vl_prj/Solid_fuel_boiler(rev\ 2_0)/Software/src/Drivers/spi.h  \
-	D:/Vl_prj/Solid_fuel_boiler(rev\ 2_0)/Software/src/DebugTools/logicAnalyzer.h  \
-	D:/Vl_prj/Solid_fuel_boiler(rev\ 2_0)/Software/src/System/stdint.h  \
-	D:/Vl_prj/Solid_fuel_boiler(rev\ 2_0)/Software/src/Main/boardModes.h  \
-
--- Software/solid_fuel_boiler.X/build/default/debug/_ext/6916191/module_analog_inputs.o.d
+++
@@ -1,5 +0,0 @@
-build/default/debug/_ext/6916191/module_analog_inputs.o:  \
-	D:/Vl_prj/Solid_fuel_boiler(rev\ 2_0)/Software/src/Module_analog_inputs/module_analog_inputs.c  \
-	D:/Vl_prj/Solid_fuel_boiler(rev\ 2_0)/Software/src/Module_analog_inputs/module_analog_inputs.h  \
-	D:/Vl_prj/Solid_fuel_boiler(rev\ 2_0)/Software/src/System/stdint.h  \
-
